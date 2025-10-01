28f9183ee50993a60e6f6aa32c041c651e5c95ae
Junin <166250057+joseMariaMartins@users.noreply.github.com>
2025-10-01 20:27:36 -0300

    FormFuncionarios
    adiciona prototipo da tela

--- telebip-erp/Forms/Main/FormFuncionarios.Designer.cs
+++ telebip-erp/Forms/Main/FormFuncionarios.Designer.cs
@@ -29,224 +29,341 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormFuncionarios));
-            flp = new FlowLayoutPanel();
-            pnlCard = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel2 = new Panel();
             label2 = new Label();
             label1 = new Label();
-            panel1 = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel5 = new Panel();
+            panel6 = new Panel();
             label3 = new Label();
             label4 = new Label();
-            panel2 = new Panel();
+            panel7 = new Panel();
             label5 = new Label();
             label6 = new Label();
-            panel3 = new Panel();
-            panel5 = new Panel();
-            panel4 = new Panel();
-            pictureBox1 = new PictureBox();
-            flp.SuspendLayout();
-            pnlCard.SuspendLayout();
+            panel8 = new Panel();
+            label7 = new Label();
+            label8 = new Label();
+            panel9 = new Panel();
+            label9 = new Label();
+            label10 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
             panel3.SuspendLayout();
             panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            panel5.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
+            panel8.SuspendLayout();
+            panel9.SuspendLayout();
             SuspendLayout();
             // 
-            // flp
+            // pictureBox1
             // 
-            flp.Controls.Add(pnlCard);
-            flp.Controls.Add(panel1);
-            flp.Controls.Add(panel2);
-            flp.Dock = DockStyle.Left;
-            flp.FlowDirection = FlowDirection.TopDown;
-            flp.Location = new Point(0, 0);
-            flp.Margin = new Padding(2);
-            flp.Name = "flp";
-            flp.Size = new Size(887, 773);
-            flp.TabIndex = 0;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(444, 410);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
             // 
-            // pnlCard
+            // panel1
             // 
-            pnlCard.BackColor = Color.FromArgb(23, 24, 29);
-            pnlCard.Controls.Add(label2);
-            pnlCard.Controls.Add(label1);
-            pnlCard.Location = new Point(20, 20);
-            pnlCard.Margin = new Padding(20, 20, 20, 0);
-            pnlCard.Name = "pnlCard";
-            pnlCard.Size = new Size(844, 77);
-            pnlCard.TabIndex = 0;
+            panel1.BackColor = Color.FromArgb(23, 24, 29);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(33, 42);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(444, 520);
+            panel1.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel4);
+            panel3.Dock = DockStyle.Fill;
+            panel3.Location = new Point(0, 110);
+            panel3.Margin = new Padding(2);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(444, 410);
+            panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.Controls.Add(pictureBox1);
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(0, 0);
+            panel4.Margin = new Padding(2);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(444, 410);
+            panel4.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(444, 110);
+            panel2.TabIndex = 0;
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             label2.ForeColor = Color.White;
-            label2.Location = new Point(34, 48);
+            label2.Location = new Point(13, 55);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(52, 19);
+            label2.Size = new Size(63, 23);
             label2.TabIndex = 1;
             label2.Text = "Efetivo";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(30, 17);
+            label1.Location = new Point(13, 18);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(292, 32);
+            label1.Size = new Size(219, 37);
             label1.TabIndex = 0;
-            label1.Text = "Ronaldo da Silva de Jesus";
+            label1.Text = "Ronaldo da Silva";
             // 
-            // panel1
+            // flowLayoutPanel1
             // 
-            panel1.BackColor = Color.FromArgb(23, 24, 29);
-            panel1.Controls.Add(label3);
-            panel1.Controls.Add(label4);
-            panel1.Location = new Point(20, 117);
-            panel1.Margin = new Padding(20, 20, 20, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(844, 77);
-            panel1.TabIndex = 2;
+            flowLayoutPanel1.Controls.Add(panel6);
+            flowLayoutPanel1.Controls.Add(panel7);
+            flowLayoutPanel1.Controls.Add(panel8);
+            flowLayoutPanel1.Controls.Add(panel9);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(708, 600);
+            flowLayoutPanel1.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            panel5.Controls.Add(panel1);
+            panel5.Dock = DockStyle.Fill;
+            panel5.Location = new Point(708, 0);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(493, 600);
+            panel5.TabIndex = 6;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(23, 24, 29);
+            panel6.Controls.Add(label4);
+            panel6.Controls.Add(label3);
+            panel6.Location = new Point(3, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(641, 117);
+            panel6.TabIndex = 0;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.White;
-            label3.Location = new Point(34, 48);
+            label3.Location = new Point(8, 17);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(52, 19);
+            label3.Size = new Size(219, 37);
             label3.TabIndex = 1;
-            label3.Text = "Efetivo";
+            label3.Text = "Ronaldo da Silva";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             label4.ForeColor = Color.White;
-            label4.Location = new Point(30, 17);
+            label4.Location = new Point(10, 57);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(292, 32);
-            label4.TabIndex = 0;
-            label4.Text = "Ronaldo da Silva de Jesus";
+            label4.Size = new Size(63, 23);
+            label4.TabIndex = 2;
+            label4.Text = "Efetivo";
             // 
-            // panel2
+            // panel7
             // 
-            panel2.BackColor = Color.FromArgb(23, 24, 29);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(label6);
-            panel2.Location = new Point(20, 214);
-            panel2.Margin = new Padding(20);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(844, 77);
-            panel2.TabIndex = 3;
+            panel7.BackColor = Color.FromArgb(23, 24, 29);
+            panel7.Controls.Add(label5);
+            panel7.Controls.Add(label6);
+            panel7.Location = new Point(3, 126);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(641, 117);
+            panel7.TabIndex = 3;
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
             label5.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
             label5.ForeColor = Color.White;
-            label5.Location = new Point(34, 48);
+            label5.Location = new Point(10, 57);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(52, 19);
-            label5.TabIndex = 1;
+            label5.Size = new Size(63, 23);
+            label5.TabIndex = 2;
             label5.Text = "Efetivo";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = Color.White;
-            label6.Location = new Point(30, 17);
+            label6.Location = new Point(8, 17);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(292, 32);
-            label6.TabIndex = 0;
-            label6.Text = "Ronaldo da Silva de Jesus";
+            label6.Size = new Size(219, 37);
+            label6.TabIndex = 1;
+            label6.Text = "Ronaldo da Silva";
             // 
-            // panel3
+            // panel8
             // 
-            panel3.Controls.Add(panel5);
-            panel3.Controls.Add(panel4);
-            panel3.Dock = DockStyle.Fill;
-            panel3.Location = new Point(887, 0);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(739, 773);
-            panel3.TabIndex = 1;
+            panel8.BackColor = Color.FromArgb(23, 24, 29);
+            panel8.Controls.Add(label7);
+            panel8.Controls.Add(label8);
+            panel8.Location = new Point(3, 249);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(641, 117);
+            panel8.TabIndex = 4;
             // 
-            // panel5
+            // label7
             // 
-            panel5.Dock = DockStyle.Fill;
-            panel5.Location = new Point(0, 427);
-            panel5.Name = "panel5";
-            panel5.Size = new Size(739, 346);
-            panel5.TabIndex = 1;
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(10, 57);
+            label7.Margin = new Padding(2, 0, 2, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(63, 23);
+            label7.TabIndex = 2;
+            label7.Text = "Efetivo";
             // 
-            // panel4
+            // label8
             // 
-            panel4.Controls.Add(pictureBox1);
-            panel4.Dock = DockStyle.Top;
-            panel4.Location = new Point(0, 0);
-            panel4.Name = "panel4";
-            panel4.Padding = new Padding(20);
-            panel4.Size = new Size(739, 427);
-            panel4.TabIndex = 0;
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.White;
+            label8.Location = new Point(8, 17);
+            label8.Margin = new Padding(2, 0, 2, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(219, 37);
+            label8.TabIndex = 1;
+            label8.Text = "Ronaldo da Silva";
             // 
-            // pictureBox1
+            // panel9
             // 
-            pictureBox1.Dock = DockStyle.Fill;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(20, 20);
-            pictureBox1.Margin = new Padding(0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(699, 387);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            panel9.BackColor = Color.FromArgb(23, 24, 29);
+            panel9.Controls.Add(label9);
+            panel9.Controls.Add(label10);
+            panel9.Location = new Point(3, 372);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(641, 117);
+            panel9.TabIndex = 5;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(10, 57);
+            label9.Margin = new Padding(2, 0, 2, 0);
+            label9.Name = "label9";
+            label9.Size = new Size(63, 23);
+            label9.TabIndex = 2;
+            label9.Text = "Efetivo";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.White;
+            label10.Location = new Point(8, 17);
+            label10.Margin = new Padding(2, 0, 2, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(219, 37);
+            label10.TabIndex = 1;
+            label10.Text = "Ronaldo da Silva";
             // 
             // FormFuncionarios
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(34, 35, 49);
-            ClientSize = new Size(1626, 773);
+            ClientSize = new Size(1201, 600);
             ControlBox = false;
-            Controls.Add(panel3);
-            Controls.Add(flp);
+            Controls.Add(panel5);
+            Controls.Add(flowLayoutPanel1);
             FormBorderStyle = FormBorderStyle.None;
             Margin = new Padding(2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FormFuncionarios";
             Text = "FormFuncionarios";
-            flp.ResumeLayout(false);
-            pnlCard.ResumeLayout(false);
-            pnlCard.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
             panel3.ResumeLayout(false);
             panel4.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            panel5.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            panel8.ResumeLayout(false);
+            panel8.PerformLayout();
+            panel9.ResumeLayout(false);
+            panel9.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private FlowLayoutPanel flp;
-        private Panel pnlCard;
-        private Label label1;
-        private Label label2;
+        private PictureBox pictureBox1;
         private Panel panel1;
-        private Label label3;
-        private Label label4;
+        private Panel panel3;
         private Panel panel2;
+        private Label label2;
+        private Label label1;
+        private Panel panel4;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel6;
+        private Label label4;
+        private Label label3;
+        private Panel panel7;
         private Label label5;
         private Label label6;
-        private Panel panel3;
+        private Panel panel8;
+        private Label label7;
+        private Label label8;
+        private Panel panel9;
+        private Label label9;
+        private Label label10;
         private Panel panel5;
-        private Panel panel4;
-        private PictureBox pictureBox1;
     }
 }
